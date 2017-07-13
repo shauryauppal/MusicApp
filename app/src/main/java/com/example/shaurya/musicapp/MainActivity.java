959837e36a8a4889181c259cc44137233221ae1c
shauryauppal <shauryauppal00111@gmail.com>
2017-07-13 18:19:00 +0530

    Simple music player app tutorial

--- app/src/main/java/com/example/shaurya/musicapp/MainActivity.java
+++ app/src/main/java/com/example/shaurya/musicapp/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.shaurya.musicapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MediaPlayer mediaplayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaplayer = MediaPlayer.create(this,R.raw.euphoria);
+        Button play = (Button) findViewById(R.id.Play);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "Play Music", Toast.LENGTH_SHORT).show();
+
+                mediaplayer.start();
+            }
+        });
+
+
+        Button pause = (Button) findViewById(R.id.Pause);
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "Pause Music", Toast.LENGTH_SHORT).show();
+                mediaplayer.pause();
+            }
+        });
     }
+
+
+
 }
